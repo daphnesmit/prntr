7f9e9f307061b931f0d235de9d0bff6b446dfa2d
Daphne Smit <daphnesmit1986@gmail.com>
2022-01-20 12:58:50 +0100

    Export prntr function

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import Prntr, { PrntrArguments } from './Prntr'
 function prntr(...args: PrntrArguments) {
   return new Prntr(...args)
 }
+
 if (typeof window !== 'undefined') {
   // @ts-ignore
   window.prntr = prntr;
 }
 
-export default Prntr
+export default prntr
